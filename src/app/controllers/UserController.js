03b8a99fa116bd76af7541327990566589cadd1a
lucasbritx <lucasbritesxavier2010@gmail.com>
2020-11-06 13:59:54 -0300

    a

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,80 +4,86 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll();
-
+      
       return res.json(users);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
+  
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-
+      
       return res.json(user);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
+  
   async store(req, res) {
     try {
       const user = await User.create({balance: 1000.00, limit: 500.00, ...req.body});
-
+      
       return res.json(user);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
+  
   async update(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-
+      
       await user.update(req.body);
-
+      
       return res.json({ user });
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
+  
   async destroy(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-
+      
       await user.destroy();
-
+      
       return res.json();
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
+  
   async withdrawMoney(transactionJSON) {
     const user = await User.findByPk(transactionJSON.senderId);
-    user.balance = Number(user.balance) - Number(transactionJSON.value);
-    const newUser = user;
-    const response = await user.update(newUser.dataValues, {
+    if((Number(user.balance) - Number(transactionJSON.value)) < 0){
+      const rest = Number(transactionJSON.value) - Number(user.balance);
+      if(Number(user.limit) - rest >= 0){
+        user.limit = Number(user.limit) - rest;
+        user.balance = Number(user.balance) - (Number(transactionJSON.value) - rest);
+      }
+    }else {
+      user.balance = Number(user.balance) - Number(transactionJSON.value);
+    }
+    const response = await user.update(user.dataValues, {
       where: {
         id: transactionJSON.userId
       }});
-  
-    return response;
-  }
-
-  async receiveMoney(transactionJSON) {
-    const user = await User.findByPk(transactionJSON.receiverId);
-    user.balance = await Number(user.balance) + Number(transactionJSON.value);
-    const newUser = user;
-    const response = await user.update(newUser.dataValues, {
-      where: {
-        id: transactionJSON.receiverId
-      }});
-  
-    return response;
-  }
-  
-}
-module.exports = new UserController();
+      
+      return response;
+    }
+    
+    async receiveMoney(transactionJSON) {
+      const user = await User.findByPk(transactionJSON.receiverId);
+      user.balance = await Number(user.balance) + Number(transactionJSON.value);
+      const response = await user.update(user.dataValues, {
+        where: {
+          id: transactionJSON.receiverId
+        }});
+        
+        return response;
+      }
+      
+    }
+    module.exports = new UserController();
